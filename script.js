5e17585f510b1f1572da313499ddbd11f622b76f
Nailgy <ilyha.nali@gmail.com>
2022-06-03 04:44:10 +0300

    deduplicated code by putting Players into Field

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const NUM_OF_PLAYERS = 2;
 const LEVELS_OF_TREASURES = [1, 2, 3, 4];
 const TREASURES_AT_1_LEVEL = 8;
 const RANGE_FOR_EACH_LEVEL = 4;
@@ -38,100 +39,10 @@ const randValueFromTreasure = (levelOfTreasure) => {
   return rand(max, min);
 };
 
-const switchPlayer = (activePlayerIndex) => {
-  const newIndex = (activePlayerIndex === 0) ? 1 : 0;
+const next = (currIndex, maxIndex, minIndex = 0) => {
+  const newIndex = (currIndex < maxIndex) ? currIndex + 1 : minIndex;
   return newIndex;
 };
-
-class Field {
-  constructor(treasuresAtOneLevel, levelsOfTreasures, maxOxygen) {
-    this.treasuresAtOneLevel = treasuresAtOneLevel;
-    this.levelsOfTreasures = levelsOfTreasures;
-    this.maxOxygen = maxOxygen;
-    this.currentOxygen = maxOxygen;
-
-    let tiles = [SUBMARINE];
-    for (const level of this.levelsOfTreasures) {
-      const obj = { value: level, isFree: true };
-      const arr = dup(obj, this.treasuresAtOneLevel);
-      tiles = tiles.concat(arr);
-    }
-    this.tiles = tiles;
-  }
-
-  resetOxygen() {
-    this.currentOxygen = this.maxOxygen;
-  }
-
-  reduceOxygen(amount) {
-    this.currentOxygen -= amount;
-  }
-
-  isOxygenLeft() {
-    return this.currentOxygen > 0;
-  }
-
-  occupyTile(index) {
-    this.tiles[index].isFree = false;
-  }
-
-  freeUpTile(index) {
-    this.tiles[index].isFree = true;
-  }
-
-  takeTresure(index) {
-    const currentTreasure = this.tiles[index].value;
-    this.tiles[index] = { ...EMPTY_TILE };
-    return currentTreasure;
-  }
-
-  replaceEmptyTile(index, treasure) {
-    this.tiles[index].value = treasure;
-    this.freeUpTile(index);
-  }
-
-  movePlayerDown(startingIndex, number, numberOfTreasures) {
-    const length = this.tiles.length;
-    this.freeUpTile(startingIndex);
-    let stepsRemain = number + numberOfTreasures;
-    let currentIndex = startingIndex;
-
-    while (stepsRemain > 0) {
-      currentIndex += DOWNWARDS;
-      if (currentIndex < length) {
-        if (!this.tiles[currentIndex].isFree) currentIndex += DOWNWARDS;
-        stepsRemain--;
-        continue;
-      }
-      if (this.tiles[length - 1].isFree) currentIndex = length - 1;
-      else currentIndex = length - 2;
-      break;
-    }
-
-    this.occupyTile(currentIndex);
-    return currentIndex;
-  }
-
-  movePlayerUp(startingIndex, number, numberOfTreasures) {
-    this.freeUpTile(startingIndex);
-    let stepsRemain = number - numberOfTreasures;
-    let currentIndex = startingIndex;
-
-    while (stepsRemain > 0) {
-      currentIndex += UPWARDS;
-      if (currentIndex >= 0) {
-        if (!this.tiles[currentIndex].isFree) currentIndex += UPWARDS;
-        stepsRemain--;
-        continue;
-      }
-      currentIndex = 0;
-      break;
-    }
-
-    if (currentIndex) this.occupyTile(currentIndex);
-    return currentIndex;
-  }
-}
 
 class Player {
   constructor(position = 0, treasures = []) {
@@ -185,74 +96,164 @@ class Player {
   }
 }
 
-const field = new Field(TREASURES_AT_1_LEVEL, LEVELS_OF_TREASURES, MAX_OXYGEN);
-const player0 = new Player();
-const player1 = new Player();
-const players = [player0, player1];
-let activePlayerIndex = 0;
-let activePlayer = players[activePlayerIndex];
+class Field {
+  constructor(treasuresAtOneLevel, levelsOfTreasures, maxOxygen, numOfPlayers) {
+    this.treasuresAtOneLevel = treasuresAtOneLevel;
+    this.levelsOfTreasures = levelsOfTreasures;
+    this.maxOxygen = maxOxygen;
+    this.currentOxygen = maxOxygen;
+
+    let tiles = [SUBMARINE];
+    for (const level of this.levelsOfTreasures) {
+      const obj = { value: level, isFree: true };
+      const arr = dup(obj, this.treasuresAtOneLevel);
+      tiles = tiles.concat(arr);
+    }
+    this.tiles = tiles;
+
+    const players = [];
+    for (let i = 0; i < numOfPlayers; i++) {
+      players.push(new Player());
+    }
+    this.players = players;
+    this.activeIndex = 0;
+    this.activePlayer = this.players[this.activeIndex];
+  }
+
+  resetOxygen() {
+    this.currentOxygen = this.maxOxygen;
+  }
+
+  reduceOxygen(amount) {
+    this.currentOxygen -= amount;
+  }
+
+  checkOxygen() {
+    if (this.currentOxygen <= 0) {
+      this.players.filter((player) => !player.isSaved).forEach((player) => {
+        player.reset();
+        console.dir(`${player} resetted `);
+      });
+    }
+  }
+
+  occupyTile(index) {
+    this.tiles[index].isFree = false;
+  }
+
+  freeUpTile(index) {
+    this.tiles[index].isFree = true;
+  }
+
+  takeTresure(index) {
+    const currentTreasure = this.tiles[index].value;
+    this.tiles[index] = { ...EMPTY_TILE };
+    return currentTreasure;
+  }
+
+  replaceEmptyTile(index, treasure) {
+    this.tiles[index].value = treasure;
+    this.freeUpTile(index);
+  }
+
+  movePlayerDown(startingIndex, number, numberOfTreasures) {
+    const length = this.tiles.length;
+    this.freeUpTile(startingIndex);
+    let stepsRemain = number + numberOfTreasures;
+    let currentIndex = startingIndex;
+
+    while (stepsRemain > 0) {
+      currentIndex += DOWNWARDS;
+      if (currentIndex < length) {
+        if (!this.tiles[currentIndex].isFree) currentIndex += DOWNWARDS;
+        stepsRemain--;
+        continue;
+      }
+      currentIndex = (this.tiles[length - 1].isFree) ? length - 1 : length - 2;
+      break;
+    }
+
+    this.occupyTile(currentIndex);
+    return currentIndex;
+  }
+
+  movePlayerUp(startingIndex, number, numberOfTreasures) {
+    this.freeUpTile(startingIndex);
+    let stepsRemain = number - numberOfTreasures;
+    let currentIndex = startingIndex;
+
+    while (stepsRemain > 0) {
+      currentIndex += UPWARDS;
+      if (currentIndex >= 0) {
+        if (!this.tiles[currentIndex].isFree) currentIndex += UPWARDS;
+        stepsRemain--;
+        continue;
+      }
+      currentIndex = 0;
+      break;
+    }
+
+    if (currentIndex) this.occupyTile(currentIndex);
+    return currentIndex;
+  }
+
+  swapPlayer() {
+    this.activeIndex = next(this.activeIndex, this.players.length - 1);
+    this.activePlayer = this.players[this.activeIndex];
+    console.dir('--- SWAP TO ---     Player' + this.activeIndex);
+  }
+}
+
+const field = new Field(TREASURES_AT_1_LEVEL, LEVELS_OF_TREASURES, MAX_OXYGEN, NUM_OF_PLAYERS);
 //now do only js mechanics of game, html & css realisation will be added later
 
 //making skip and take button inactive in css
 btnMoveUp.addEventListener('click', () => {
-  activePlayer.moveUp();
+  field.activePlayer.moveUp();
+  console.dir('Player' + field.activeIndex + ' is moving up');
 });
 
 btnRoll.addEventListener('click', () => {
   const value = rollTwoDices(DICE_VALUES);
   console.dir(value + ' on dices');
   //showing rolled dices on screen
-  const dir = activePlayer.direction;
-  const pos = activePlayer.position;
-  const num = activePlayer.numberOfTreasures();
+  const dir = field.activePlayer.direction;
+  const pos = field.activePlayer.position;
+  const num = field.activePlayer.numberOfTreasures();
   field.reduceOxygen(num);
-  console.dir(field.currentOxygen + ' oxygen');
+  console.dir(field.currentOxygen + ' oxygen left \n');
+
+  let newPos;
   if (dir === DOWNWARDS) {
-    const newPos = field.movePlayerDown(pos, value, num);
-    activePlayer.changePos(newPos);
-    console.dir('new placement of ' + activePlayerIndex + ' is ' + newPos);
-    //moving player to new position on screen
+    newPos = field.movePlayerDown(pos, value, num);
   } else {
-    const newPos = field.movePlayerUp(pos, value, num);
-    activePlayer.changePos(newPos);
-    console.dir('new placement of ' + activePlayerIndex + ' is ' + newPos);
-    //moving player to new position on screen
+    newPos = field.movePlayerUp(pos, value, num);
   }
-  if (activePlayer.position === 0) {
-    activePlayer.save();
-    console.dir('player ' + activePlayerIndex + ' saved and got ' + activePlayer.totalPoints);
+  field.activePlayer.changePos(newPos);
+  console.dir('new placement of Player' + field.activeIndex + ' is ' + newPos);
+  //moving player to new position on screen
+
+  if (field.activePlayer.position === 0) {
+    field.activePlayer.save();
+    console.dir('Player' + field.activeIndex + ' saved and got ' + field.activePlayer.totalPoints);
     //display totalPoints of activePlayer;
   }
 });
 
 btnTake.addEventListener('click', () => {
-  const pos = activePlayer.position;
+  const pos = field.activePlayer.position;
   const treasure = field.takeTresure(pos);
-  if (treasure > 0) activePlayer.addTreasure(treasure);
+  if (treasure > 0) field.activePlayer.addTreasure(treasure);
   //removing treasure from field and giving it to player
-  console.dir(activePlayerIndex + 'got a treasure ' + treasure);
-  if (!field.isOxygenLeft()) {
-    players.filter((player) => !player.isAboard()).forEach((player) => {
-      player.reset();
-      console.dir(`${player} resetted `);
-    });
-  }
-  activePlayerIndex = switchPlayer(activePlayerIndex);
-  activePlayer = players[activePlayerIndex];
-  console.dir('--- SWAP TO --- ' + activePlayerIndex);
+  console.dir('Player' + field.activeIndex + ' got a treasure ' + treasure);
+  field.checkOxygen();
+  field.swapPlayer();
   //blurring old player and making active another
 });
 
 btnSkip.addEventListener('click', () => {
-  if (!field.isOxygenLeft()) {
-    players.filter((player) => !player.isAboard).forEach((player) => {
-      player.reset;
-      console.dir(`${player} resetted `);
-    });
-  }
-  activePlayerIndex = switchPlayer(activePlayerIndex);
-  activePlayer = players[activePlayerIndex];
-  console.dir('--- SWAP TO --- ' + activePlayerIndex);
+  field.checkOxygen();
+  field.swapPlayer();
   //blurring old player and making active another
 });
 
